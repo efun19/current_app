d4aa5657e535fb63276a859892377d17dd809ed4
ljm <lijiaming@wedobest.com.cn>
2025-07-08 15:18:49 +0800

    添加获取应用信息的工具类

--- app/src/main/java/com/daim/topactivity/MainActivity.java
+++ app/src/main/java/com/daim/topactivity/MainActivity.java
@@ -3,9 +3,6 @@ package com.daim.topactivity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
-import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
 
 import android.app.ActivityManager;
 import android.content.Context;
@@ -13,11 +10,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
---
+++ app/src/main/java/com/daim/topactivity/PackageInfoUtil.java
@@ -0,0 +1,47 @@
+package com.daim.topactivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ljm
+ * @description
+ * @date
+ */
+public class PackageInfoUtil {
+    private static final String TAG = "PackageInfoUtil";
+
+    /**
+     * 获取应用信息
+     * @param context
+     */
+    public static void getPackageInfo(Context context) {
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN);
+        List<ResolveInfo> mApps = context.getPackageManager().queryIntentActivities(mainIntent, 0);
+        PackageManager packageManager = context.getApplicationContext().getPackageManager();
+        List<String> packageList = new ArrayList<>();
+        for (ResolveInfo appInfo: mApps) {
+            if (packageList.contains(appInfo.activityInfo.packageName)) {
+                continue;
+            }
+            packageList.add(appInfo.activityInfo.packageName);
+            String appLabel = appInfo.loadLabel(packageManager).toString();
+            String packageName = appInfo.activityInfo.packageName;
+            String startAct = appInfo.activityInfo.name;
+            String appIcon = appInfo.loadIcon(packageManager).toString();
+            try {
+                PackageInfo packageInfo = packageManager.getPackageInfo(packageName,0);
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.d(TAG, "get package info error : " + e.getMessage());
+            }
+        }
+        Log.d(TAG, "getPackageInfo: " + packageList);
+    }
+}
